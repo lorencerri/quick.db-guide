4da6c76c85faeabc9caa9d7faa2d70b72b4f4e88
Loren Cerri <lorencerri@gmail.com>
2022-05-30 04:58:49 -0400

    Update DemoApp.jsx

--- src/steps/DemoApp.jsx
+++ src/steps/DemoApp.jsx
@@ -7,22 +7,41 @@ const code = `const { QuickDB } = require("quick.db");
 const db = new QuickDB();
 
 async function incrementBalance() {
-	await db.add("myUser.balance", 100);
+    await db.add("myUser.balance", 100);
 }
 
 async function decrementBalance() {
-	await db.sub("myUser.balance", 25);
+    await db.sub("myUser.balance", 25);
 }
 
 async function addRandomItem() {
-	let items = ['Sword', 'Axe', 'Armor', 'Amulet', 'Ring', 'Boots', 'Helmet', 'Shield'];
-	await db.push("myUser.items", items[Math.floor(Math.random() * items.length)]);
+    let items = ['Sword', 'Axe', 'Armor', 'Amulet', 'Ring', 'Boots', 'Helmet', 'Shield'];
+    await db.push("myUser.items", items[Math.floor(Math.random() * items.length)]);
+}
+
+async function deleteUser() {
+    await db.delete("myUser");
 }
 `;
 
 export const DemoApp = () => {
-    const [balance, setBalance] = useState(0);
+    const [balance, setBalance] = useState(null);
     const [items, setItems] = useState([]);
+
+    const getOutput = () => {
+        let balanceStr =
+            balance !== null
+                ? ` balance: ${balance}${items.length > 0 ? "," : ""} `
+                : "";
+        let itemsStr =
+            items.length > 0
+                ? `${balance === null ? " " : ""}items: [${items.join(", ")}] `
+                : "";
+
+        if (balance !== null || items.length > 0)
+            return `{${balanceStr}${itemsStr}}`;
+        else return "undefined";
+    };
 
     return (
         <div style={{ margin: "10px" }}>
@@ -36,7 +55,9 @@ export const DemoApp = () => {
                 <Button
                     color="green"
                     onClick={() => {
-                        setBalance((state) => state + 100);
+                        setBalance((state) =>
+                            state === null ? 100 : state + 100
+                        );
                     }}
                 >
                     incrementBalance()
@@ -70,14 +91,21 @@ export const DemoApp = () => {
                 >
                     addRandomItem()
                 </Button>
+                <Button
+                    color="green"
+                    onClick={() => {
+                        setItems([]);
+                        setBalance(null);
+                    }}
+                >
+                    deleteUser()
+                </Button>
             </Group>
             <Text mt="sm" weight={700}>
                 Output
             </Text>
             <Prism withLineNumbers noCopy language="js">
-                {`let output = await db.get('myUser')\n// Output -> { balance: ${balance}, items: [${items.join(
-                    ", "
-                )}] }`}
+                {`let output = await db.get('myUser')\n// Output ->  ${getOutput()}`}
             </Prism>
         </div>
     );
