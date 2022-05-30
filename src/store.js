c30bc73b4d5bf316e7c0ab75441e7c59e30b3820
Loren Cerri <lorencerri@gmail.com>
2022-05-30 18:37:36 -0400

    Update store.js

--- src/store.js
+++ src/store.js
@@ -4,12 +4,13 @@ const useStore = create(set => ({
 	steps: [
 		{ label: "Introduction", path: "" },
 		{ label: "Installation", path: "installation" },
+		{ label: "Migration Guide", path: "migration" },
 		{ label: "Basic Usage", path: "basic-usage" },
 		{ label: "Tables", path: "tables" },
 		{ label: "Using with Discord.js", path: "using-with-discordjs" },
 		{ label: "Demo App", path: "demo-app" },
 		{ label: "API Docs", path: "api" },
-		{ label: "Migration Guide", path: "migration" }],
+	],
 
 	step: 0,
 	opened: false,
