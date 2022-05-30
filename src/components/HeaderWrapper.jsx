048c48dace59bf527c25786f29dfdcbdf6344ff0
Loren Cerri <lorencerri@gmail.com>
2022-05-30 01:06:46 -0400

    Add Discord Button

--- src/components/HeaderWrapper.jsx
+++ src/components/HeaderWrapper.jsx
@@ -1,4 +1,13 @@
-import { Header, Group, Text, MediaQuery, Burger } from "@mantine/core";
+import {
+    Header,
+    Group,
+    Text,
+    MediaQuery,
+    Burger,
+    Anchor,
+    Button,
+} from "@mantine/core";
+import { BrandDiscord } from "tabler-icons-react";
 import { useStore } from "../store";
 
 const style = { fontFamily: "Source Sans Pro" };
@@ -9,18 +18,29 @@ export const HeaderWrapper = () => {
 
     return (
         <Header height={60}>
-            <Group p="13px" spacing="sm">
-                <MediaQuery largerThan="sm" styles={{ display: "none" }}>
-                    <Burger
-                        opened={opened}
-                        onClick={() => setOpened(!opened)}
-                        size="sm"
-                        mr="xl"
-                    />
-                </MediaQuery>
-                <Text style={style} size="xl" weight={700}>
-                    quick.db guide
-                </Text>
+            <Group position="apart">
+                <Group p="13px" spacing="sm">
+                    <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+                        <Burger
+                            opened={opened}
+                            onClick={() => setOpened(!opened)}
+                            size="sm"
+                            mr="xl"
+                        />
+                    </MediaQuery>
+                    <Text style={style} size="xl" weight={700}>
+                        quick.db guide
+                    </Text>
+                </Group>
+                <Anchor href="https://discord.gg/plexidev" target="_blank">
+                    <Button
+                        style={{ background: "#5865F2" }}
+                        leftIcon={<BrandDiscord size={18} />}
+                        mr="sm"
+                    >
+                        Discord
+                    </Button>
+                </Anchor>
             </Group>
         </Header>
     );
